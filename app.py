5525a36d27f366a584d81ed9fe877d3d3af64021
Youssef Layechi <100000912+Yousseflayechi@users.noreply.github.com>
2024-01-11 15:58:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,17 @@ from tkinter import *
 from tkinter import ttk
 import tkcalendar
 import psycopg2 as pg2
+import tksheet
+from tkinter import messagebox
 
 class App:
     def __init__(self):
         root = Tk()
         root.title("APPLICATION")
+        root.geometry("700x800")
 
         # Database Connection
-        conn = pg2.connect(database='__', user='___', password='___')
+        conn = pg2.connect(database='user_info', user='postgres', password='YOURPASSWORD')
         cur = conn.cursor()
 
         # Create a table if it doesn't exist
@@ -17,7 +20,7 @@ class App:
                     "first_name VARCHAR(50), last_name VARCHAR(50), "
                     "gender VARCHAR(10), age INT, nationality VARCHAR(50), "
                     "born_date DATE, phone_number VARCHAR(15), email VARCHAR(50), "
-                    "country VARCHAR(50), city VARCHAR(50), zipcode VARCHAR(10))")
+                    "country VARCHAR(50), city VARCHAR(50), zipcode VARCHAR(50))")
         conn.commit()
 
         frame = Frame(root)
@@ -26,7 +29,6 @@ class App:
         label_frame = LabelFrame(frame, text='User Infos')
         label_frame.grid(row=0, column=0, padx=10, pady=10)
 
-        # Labels for User Infos
         Label(label_frame, text='ID').grid(row=0, column=0)
         Label(label_frame, text='First Name').grid(row=0, column=1)
         Label(label_frame, text='Last Name').grid(row=0, column=2)
@@ -35,7 +37,6 @@ class App:
         Label(label_frame, text='Nationality').grid(row=0, column=5)
         Label(label_frame, text='Born date').grid(row=2, column=0)
 
-        # Entry widgets for User Infos
         id_entry = Entry(label_frame)
         first_name_entry = Entry(label_frame)
         last_name_entry = Entry(label_frame)
@@ -62,14 +63,12 @@ class App:
         label_frame2 = LabelFrame(frame2, text='Another Infos')
         label_frame2.grid(row=0, column=0, padx=10, pady=10)
 
-        # Labels for Another Infos
         Label(label_frame2, text='Phone number').grid(row=0, column=0)
         Label(label_frame2, text='Email').grid(row=0, column=1)
         Label(label_frame2, text='Country').grid(row=2, column=0)
         Label(label_frame2, text='City').grid(row=2, column=1)
         Label(label_frame2, text='Zipcode').grid(row=2, column=2)
 
-        # Entry widgets for Another Infos
         email_entry = Entry(label_frame2)
         phone_entry = Entry(label_frame2)
         country_entry = Entry(label_frame2)
@@ -94,19 +93,98 @@ class App:
             conn, cur))
         save_button.pack()
 
+        frame3 = Frame(root)
+        frame3.pack()
+
+        label_frame3 = LabelFrame(frame3, text='View and Update User Infos')
+        label_frame3.grid(row=0, column=0, padx=10, pady=10)
+
+        id_label = Label(label_frame3, text="Enter Id")
+        id_label.grid(row=1, column=1)
+
+        id_entry2_delete = Entry(label_frame3)
+        id_entry2_delete.grid(row=1, column=2)
+
+        deleteButton = Button(root, text="delete", command=lambda: self.delete_data(id_entry2_delete.get(), cur, conn))
+        deleteButton.pack()
+
+        frame4 = Frame(root)
+        frame4.pack()
+        label_frame4 = LabelFrame(frame4, text='Show Data')
+        label_frame4.grid(row=0, column=0, padx=10, pady=30)
+
+        showButton = Button(label_frame4, text="Show Data", command=lambda: self.show(cur, conn))
+        showButton.pack()
+
         root.mainloop()
 
     def save_data(self, id_val, first_name, last_name, gender, age, nationality,
-                  born_date, phone_number, email, country, city, zipcode, conn, cur):
+                  born_date, email, phone_number, country, city, zipcode, conn, cur):
         try:
             query = "INSERT INTO user_info (id, first_name, last_name, gender, age, nationality, " \
-                    "born_date, phone_number, email, country, city, zipcode) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+                    "born_date, email, phone_number, country, city, zipcode) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
             values = (id_val, first_name, last_name, gender, age, nationality, born_date,
-                      phone_number, email, country, city, zipcode)
+                      email, phone_number, country, city, zipcode)
             cur.execute(query, values)
             conn.commit()
-            print("Data saved successfully!")
+            messagebox.showinfo("showinfo", "Data saved successfully!")
+        except Exception as e:
+            messagebox.showerror("showinfo", f"Error:{e}.")
+
+    def delete_data(self, user_id, cur, conn):
+        try:
+            if user_id:
+                query = "DELETE FROM user_info WHERE id = %s"
+                values = (user_id,)
+                cur.execute(query, values)
+                conn.commit()
+                messagebox.showinfo("showinfo", f"User with ID {user_id} has been deleted.")
+
+            else:
+                messagebox.showinfo("showinfo", f"User ID not provided..")
+
+        except Exception as e:
+            messagebox.showerror("showinfo", f"Error:{e}.")
+
+
+
+    def show(self, cur, conn):
+        new_window = Toplevel()
+        new_window.title("Data")
+
+        try:
+            cur.execute("SELECT * FROM user_info")
+            rows = cur.fetchall()
+            conn.commit()
         except Exception as e:
             print(f"Error: {e}")
+
+        # Create a tksheet widget
+        sheet = tksheet.Sheet(new_window)
+        sheet.grid(sticky="nsew")
+
+        # Set the headers
+        headers = ["id", "first_name", "last_name", "gender", "age", "nationality",
+                   "born_date", "phone_number", "email", "country", "city", "zipcode"]
+        sheet.headers(headers)
+
+        # Populate the data
+        sheet.set_sheet_data(rows)
+
+        # Enable sheet bindings
+        sheet.enable_bindings(("single_select",
+                               "row_select",
+                               "column_width_resize",
+                               "arrowkeys",
+                               "right_click_popup_menu",
+                               "rc_select",
+                               "rc_insert_row",
+                               "rc_delete_row",
+                               "copy",
+                               "cut",
+                               "paste",
+                               "delete",
+                               "undo",
+                               "edit_cell"))
 
 App()
